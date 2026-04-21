78b311aec5620038074c441216c3e135cf729eae
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-04-21 11:18:37 +0700

    bool delnode

--- singlelink.cpp
+++ singlelink.cpp
@@ -65,7 +65,7 @@ public:
     }
 
     bool search(int nim, Node *&previous, Node *&current)
-{
+    {
     previous = START;
     current = START;
 
@@ -76,5 +76,20 @@ public:
     }
 
     return (current != NULL);
-}
+    }
 
+bool delNode(int nim)
+{
+    Node *current, *previous;
+
+    if (!search(nim, previous, current))
+        return false;
+
+    if (current == START)
+        START= START->next;
+    else
+       previous->next = current->next;
+
+    delete current;
+    return true;
+}
