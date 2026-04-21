e982cb35d930dd4c5d485da63b8537848811f86d
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-04-21 10:57:33 +0700

    void menambahkan node

--- singlelink.cpp
+++ singlelink.cpp
@@ -19,3 +19,17 @@ public:
         START = NULL;
     }
 
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diizinkan\n";
