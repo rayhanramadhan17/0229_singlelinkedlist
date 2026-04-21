a9cf098ef49b26a745e3fc7a00a551cf140ad54a
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-04-21 11:09:08 +0700

    provious dann current

--- singlelink.cpp
+++ singlelink.cpp
@@ -33,3 +33,33 @@ public:
             if (START != NULL && nim == START->noMhs)
             {
                 cout << "\nDuplikasi noMhs tidak diizinkan\n";
+                return;
+            }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+    
+    bool listempty()
+    {
+        return (START == NULL);
+    }
+    
